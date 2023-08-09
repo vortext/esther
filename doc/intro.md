14ad10ad3eb3f94bfb8f372138b1f19af55d3f4a
Joel Kuiper <me@joelkuiper.eu>
2023-08-09 13:17:50 +0200

    Project structure

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to joelkuiper/esther
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/vortext/esther_test.clj
@@ -0,0 +1,7 @@
+(ns vortext.esther-test
+  (:require [clojure.test :refer :all]
+            [vortext.esther :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
