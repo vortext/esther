5263c0411ced5a617b0608ae5a4d75cf95fd5a38
Joel Kuiper <me@joelkuiper.eu>
2023-08-12 22:36:30 +0200

    navigator api position

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -1,33 +1,25 @@
 function setSentiment(sentimentValue) {
   sentimentValue = Math.max(0, Math.min(1, sentimentValue));
-
-  // Use the sentiment to create smooth and natural durations
-  const duration = 1.2 - sentimentValue * 0.4; // seconds
+  const duration = 1.2 - sentimentValue * 0.4;
   const ease = `cubic-bezier(${0.2 + sentimentValue * 0.3}, 0.5, 0.5, 1)`;
-
   const balls = document.querySelectorAll('.loading div');
 
-  balls.forEach(ball => {
+  balls.forEach((ball) => {
     ball.style.animationDuration = `${duration}s`;
     ball.style.animationTimingFunction = ease;
   });
 }
 
 function getSentimentEnergy() {
-  const lastMemory = document.querySelectorAll("#history .memory:last-child");
-  if (lastMemory.length) {
-    return parseFloat(lastMemory[0].dataset.energy || 0.5);
-  } else {
-    return 0.5;
-  }
+  const lastMemory = document.querySelector("#history .memory:last-child");
+  return lastMemory ? parseFloat(lastMemory.dataset.energy || 0.5) : 0.5;
 }
 
 function getTimeOfDay() {
   const date = new Date();
 
-  // FIXME Assuming a generic latitude and longitude
-  const latitude = 51.509865; // Example: London's latitude
-  const longitude = -0.118092; // Example: London's longitude
+  const latitude = window.latitude;
+  const longitude = window.longitude ;
 
   const times = SunCalc.getTimes(date, latitude, longitude);
 
@@ -68,3 +60,19 @@ function afterConverseRequest() {
   document.getElementById('user-input').focus();
   document.getElementById('user-input').scrollIntoView({behavior: 'smooth'});
 }
+
+function setPosition(lat, lon) {
+  window.latitude = lat;
+  window.longitude = lon;
+}
+
+// Geolocation Handling
+navigator.geolocation.getCurrentPosition(
+  (position) => {
+    setPosition(position.coords.latitude, position.coords.longitude);
+  },
+  (error) => {
+    setPosition(51.509865, -0.118092) ;; London
+    console.warn('Geolocation error:', error);
+  }
+);
