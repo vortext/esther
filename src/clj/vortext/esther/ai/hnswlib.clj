379584f7292162851eb7b19eebcff1192a519752
Joel Kuiper <me@joelkuiper.eu>
2023-09-07 16:25:01 +0200

    [WIP] hnswlib stuff still

--- src/clj/vortext/esther/ai/hnswlib.clj
+++ src/clj/vortext/esther/ai/hnswlib.clj
@@ -6,21 +6,17 @@
    [clojure.tools.logging :as log])
   )
 
-(def header-path
-  (str (fs/canonicalize (fs/path "native/hnswlib/bindings.h"))))
+(comment
+  (def header-path
+    (str (fs/canonicalize (fs/path "native/hnswlib/bindings.h"))))
 
-(def library-path
-  (str (fs/canonicalize (fs/path "native/hnswlib/lib/libhnsw.so"))))
+  (def library-path
+    (str (fs/canonicalize (fs/path "native/hnswlib/lib/libhnsw.so"))))
 
-(def hnswlib
-  (com.sun.jna.NativeLibrary/getInstance library-path))
+  (def hnswlib
+    (com.sun.jna.NativeLibrary/getInstance library-path))
 
-(def api (clong/easy-api header-path))
+  (def api (clong/easy-api header-path))
 
-(gen/def-api hnswlib api)
-
-(def dim 128) ; Example dimension
-(def max-elements 10000) ; Example max elements
-(def M 16) ; Example M value
-(def ef-construction 200) ; Example ef_construction value
-
+  (gen/def-api hnswlib api)
+  )
